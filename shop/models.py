748bfd30afe3b29ac5eb6feb5143743f43133871
Oleh <85799629+olehmatskiv@users.noreply.github.com>
2023-05-09 12:15:47 +0300

    create Context processors

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -13,7 +13,8 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
-
+    def get_url(self):
+        return reverse('products_by_category' , args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -34,6 +35,9 @@ class Product(models.Model):
         ordering = ('name' , )
         verbose_name = 'product'
         verbose_name_plural = 'products'
+
+    def get_url(self):
+        return reverse('product_detail' , args=[self.category.slug , self.slug])
 
     def __str__(self):
         return self.name
--- shop/views.py
+++ shop/views.py
@@ -16,5 +16,9 @@ def home(request , category_slug = None):
 
 
 
-def product(request):
-    return render(request ,  "product.html")
+def product(request , category_slug , product_slug):
+    try:
+        product = Product.objects.get(category__slug = category_slug , slug = product_slug)
+    except Exception as ex:
+        raise e
+    return render(request ,  "product.html" , {'product' : product})
